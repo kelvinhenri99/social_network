ec14b7b48e47942e6b6ee1a1a0310c140928eabb
guilhermeabell <gpeuvis@gmail.com>
2023-05-11 23:53:58 -0300

    fix: Resolvendo cors front-end

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -4,6 +4,8 @@ export function getHeaders(method = 'GET', token: string, body?: any): AxiosRequ
   return {
     headers: {
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
       Authorization: `Bearer ${token}`,
     },
     method,
